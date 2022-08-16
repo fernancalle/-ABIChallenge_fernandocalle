d1e02aac3457300b00222df4a3c55f0f3e116e88
Fernando Calle <85374880+fernancalle@users.noreply.github.com>
2022-08-16 12:10:50 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,27 +1,25 @@
-# Import dependencies
+# Import libraries
 import pandas as pd
 
-# Load the dataset in a dataframe object and include only four features as mentioned
-url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-df = pd.read_csv(url)
-include = ['Age', 'Sex', 'Embarked', 'Survived'] # Only four features
-df_ = df[include]
+# read csv file
+df = pd.read_csv('train.csv')
+df = df['Age', 'Sex', 'Embarked', 'Survived'] # Only four features
 
 # Data Preprocessing
 categoricals = []
-for col, col_type in df_.dtypes.iteritems():
+for col, col_type in df.dtypes.iteritems():
      if col_type == 'O':
           categoricals.append(col)
      else:
-          df_[col].fillna(0, inplace=True)
+          df[col].fillna(0, inplace=True)
 
-df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+df_pro = pd.get_dummies(df, columns=categoricals, dummy_na=True)
 
 # Logistic Regression classifier
 from sklearn.linear_model import LogisticRegression
 dependent_variable = 'Survived'
-x = df_ohe[df_ohe.columns.difference([dependent_variable])]
-y = df_ohe[dependent_variable]
+x = df_pro[df_pro.columns.difference([dependent_variable])]
+y = df_pro[dependent_variable]
 lr = LogisticRegression()
 lr.fit(x, y)
 
